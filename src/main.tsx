49eb01edc2d9706775e37cc5be8cbbe519b21a27
KevinVillca <kevtorricov@gmail.com>
2025-09-02 16:52:57 -0400

    feat: Se añadio y modifico las tarjeta, también se eliminaron archivos inservibles

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+
 import { AppRouter } from "./routes/AppRouter.tsx";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { AlertProvider, AuthProvider } from "./contexts/index.ts";
@@ -27,7 +26,6 @@ createRoot(document.getElementById("root")!).render(
         <AlertProvider>
           <AppRouter />
           <CssBaseline />
-          <App />
         </AlertProvider>
       </AuthProvider>
     </ThemeProvider>
